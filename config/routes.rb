c55bbe82f2ad42e66f872334a8cf8e462549f706
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-17 09:58:25 +0330

    Setup the first hellrails application.

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'pages#hello'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
